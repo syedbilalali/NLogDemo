1c761b7fd08b5a4805db2b21c3151dcf518186bb
Syed Bilal Ali <syedbilalali.dev@gmail.com>
2020-08-21 01:40:38 +0530

    Adding NLog to Console_Applocation

--- NLogDemo/Program.cs
+++ NLogDemo/Program.cs
@@ -2,13 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using NLog;
 
 namespace NLogDemo
 {
     class Program
     {
+        private static readonly Logger _logger = LogManager.GetCurrentClassLogger();
         static void Main(string[] args)
         {
+            Console.WriteLine(" ----- Starting Application -------" + AppDomain.CurrentDomain.BaseDirectory);
+            //Log the way 
+            _logger.Error(" OH ! Error is Here.....");
+            Console.WriteLine("---- Going to End Application ------->");
+            Console.ReadKey();
         }
     }
 }
